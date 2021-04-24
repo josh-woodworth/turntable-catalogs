1cf66709c7b827730428a7c8f4533e8cc9a3ae84
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-04-24 13:16:13 -0600

    Adding Files for Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,31 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do
+    if logged_in?
+      redirect "/user/#{current_user.slug}"
+    else
+      erb :'/homepage'
+    end
+  end
+
+  get '/about' do
+      erb :'/about'
+  end
+  
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
   end
 
 end
